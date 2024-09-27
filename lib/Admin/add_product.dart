af926ed5ab246653cb80f07f51db9f621e082705
TanTin <tantincva@gmail.com>
2024-09-27 12:08:44 +0700

    add-P-Admin

--- lib/Admin/add_product.dart
+++ lib/Admin/add_product.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:random_string/random_string.dart';
+import 'package:shopping_app_1/services/database.dart';
 import 'package:shopping_app_1/widget/support_widget.dart';
 
 class AddProduct extends StatefulWidget {
@@ -9,6 +16,47 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final ImagePicker _picker = ImagePicker();
+  File? selectedImage;
+  TextEditingController namecontroller = new TextEditingController();
+
+  Future<void> requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.photos.request();
+  }
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    selectedImage = File(image!.path);
+    setState(() {});
+  }
+
+  uploadItem() async {
+    if (selectedImage != null && namecontroller.text != "") {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("blogImage").child(addId);
+
+      final UploadTask tack = firebaseStorageRef.putFile(selectedImage!);
+      var downloadUrl = await (await tack).ref.getDownloadURL();
+
+      Map<String, dynamic> addProduct = {
+        "Name": namecontroller.text,
+        "Image": downloadUrl,
+      };
+      await DatabaseMethods().addProduct(addProduct, value!).then((value) {
+        selectedImage = null;
+        namecontroller.text = "";
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              "Product has been uploaded Successfully!!",
+              style: TextStyle(fontSize: 20.0),
+            )));
+      });
+    }
+  }
+
   String? value;
   final List<String> categoryitem = ['Watch', 'Laptop', 'TV', 'Headphone'];
   @override
@@ -39,16 +87,35 @@ class _AddProductState extends State<AddProduct> {
             SizedBox(
               height: 20.0,
             ),
-            Center(
-              child: Container(
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 1.5),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Icon(Icons.camera_alt_outlined),
-              ),
-            ),
+            selectedImage == null
+                ? GestureDetector(
+                    onTap: () {
+                      getImage();
+                    },
+                    child: Center(
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 1.5),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Icon(Icons.camera_alt_outlined),
+                      ),
+                    ),
+                  )
+                : Center(
+                    child: Material(
+                      elevation: 4.0,
+                      borderRadius: BorderRadius.circular(20),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                              height: 150,
+                              width: 150,
+                              child: Image.file(selectedImage!,
+                                  fit: BoxFit.cover))),
+                    ),
+                  ),
             SizedBox(
               height: 20.0,
             ),
@@ -66,6 +133,7 @@ class _AddProductState extends State<AddProduct> {
                   color: Color(0xFFececf8),
                   borderRadius: BorderRadius.circular(20)),
               child: TextField(
+                controller: namecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
@@ -111,7 +179,9 @@ class _AddProductState extends State<AddProduct> {
             ),
             Center(
               child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    uploadItem();
+                  },
                   child: Text(
                     "Add Product",
                     style: TextStyle(fontSize: 22.0),
